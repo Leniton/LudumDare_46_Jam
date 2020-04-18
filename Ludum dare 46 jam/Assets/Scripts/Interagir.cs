23818e1c2302ba17377a4e34a5a8756a0e8b98eb
Leniton <48127860+Leniton@users.noreply.github.com>
2020-04-18 17:02:20 -0300

    UI

---
+++ Ludum dare 46 jam/Assets/Scripts/Interagir.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interagir : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BarraDeProgresso, BotaoInteragir;
+    bool Interagindo = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (BotaoInteragir.activeSelf)
+        {
+            BotaoInteragir.transform.position = transform.GetChild(0).position;
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                BotaoInteragir.SetActive(false);
+                GetComponent<TopDownMovement>().enabled = false;
+                BarraDeProgresso.SetActive(true);
+                BarraDeProgresso.transform.position = transform.GetChild(0).position;
+                StartCoroutine(ProgressoInteracao(1));
+
+            }
+        }
+    }
+
+    IEnumerator ProgressoInteracao(float duracao)
+    {
+        do
+        {
+            yield return new WaitForSeconds(duracao / 40);
+            BarraDeProgresso.GetComponent<Slider>().value += 1.0f/40;
+
+        } while (BarraDeProgresso.GetComponent<Slider>().value < 1);
+
+
+        GetComponent<TopDownMovement>().enabled = true;
+        BarraDeProgresso.SetActive(false);
+        BarraDeProgresso.GetComponent<Slider>().value = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BotaoInteragir.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        BotaoInteragir.SetActive(false);
+    }
+}
---
+++ Ludum dare 46 jam/Assets/Scripts/UI_Control.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Control : MonoBehaviour
+{
+    [SerializeField]
+    Slider Sanidade, Estamina;
+    [SerializeField]
+    TextMeshProUGUI Dias, Tempo;
+    int Dia,Minuto;
+
+    private void Start()
+    {
+        StartCoroutine(ContarTempo());
+    }
+
+    IEnumerator ContarTempo()
+    {
+        do
+        {
+
+            yield return new WaitForSeconds(1);
+
+            Minuto += 1;
+
+            if((Minuto / 60) == 24)
+            {
+                Dia++;
+                Minuto = 0;
+            }
+
+            AtualizarTempo();
+
+        } while (Dia < 40);
+    }
+
+    void AtualizarTempo()
+    {
+        Tempo.text = Mathf.FloorToInt(Minuto/60).ToString("D2") +":"+ (Minuto%60).ToString("D2");
+        Dias.text = "Dia: " + Dia;
+    }
+
+    public void MudarSanidade(float Quantidade)
+    {
+        Sanidade.value += Quantidade;
+    }
+
+    public void MudarEstamina(float Quantidade)
+    {
+        Estamina.value += Quantidade;
+    }
+}
